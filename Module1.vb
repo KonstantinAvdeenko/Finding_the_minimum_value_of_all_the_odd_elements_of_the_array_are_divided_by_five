f09ba037119c2a39bb18197a4ffbeea8efaf49e4
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-03-23 18:21:55 +0400

    Add my finding the minimum value of all the odd elements of the array are divided by five

---
+++ Module1.vb
@@ -0,0 +1,22 @@
+﻿Private Sub CommandButton1_Click()
+'заполняет элементы массива рандомными положительными и отрицательными значениями от -1000 до 1000'
+For i = 1 To 20
+Cells(1, i) = Int((2000 * Rnd) - 1000)
+Next i
+End Sub
+
+Private Sub CommandButton2_Click()
+'находит и выводит минимальное значение из всех нечетных элементов массива, которые делятся на 5'
+Min = 1001
+For i = 1 To 20
+If Cells(1, i) Mod 2 <> 0 And Cells(1, i) Mod 5 = 0 And Cells(1, i) < Min Then
+Min = Cells(1, i)
+End If
+Next i
+MsgBox (Min)
+End Sub
+
+Private Sub CommandButton3_Click()
+'закрывает форму'
+UserForm1.Hide
+End Sub
---
+++ READ_ME_ENG.md
@@ -0,0 +1,8 @@
+﻿This program in the VBA programming language, finds and displays the minimum value of all odd
+array elements, which are divided by 5.
+The main module Module1 contains the following main functions corresponding to the main buttons on the form:
+1) array button - fills array elements with random positive and
+negative values ​​from -1000 to 1000;
+2) the solution button - finds and displays the minimum value from all odd elements of the array,
+which are divided by 5;
+3) exit button - closes the form.
